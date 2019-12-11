00458516452ae9b1b541066d01c6f7cb7f44c6f0
Gabriel Felipe <gabriel.gomes@ccc.ufcg.edu.br>
2019-12-11 15:27:10 -0300

    size of graph and size of population now is a parameter and change output format

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <time.h>
 #include <cstdlib>
+#include <string.h>
 #include "population.h"
 
-int main(){
+int main(int argc, char *argv[]){
+    if(argc != 4){
+        std::cout << "./run graphSize populationSize epochs" << std::endl;
+        exit(0);
+    }
+    int graphSize = atoi(argv[1]);
+    int populationSize = atoi(argv[2]);
+    int epochs = atoi(argv[3]);
     srand(time(NULL));
     Population p;
-    p.start(100, 1000);
-    for(int i = 0; i < 2000; i++){
+    p.start(graphSize, populationSize);
+    std::vector <long long> parcialResults;
+    for(int i = 0; i < epochs; i++){
         if(i%100 == 0){
-            std::cout << p.currentBest << std::endl;
+            parcialResults.emplace_back(p.currentBest);
         }
         p.nextGeneration();
     }
-    std::cout << "Final result:" << std::endl;
-    std::cout << p.currentBest << std::endl;
+    parcialResults.emplace_back(p.currentBest);
+    std::cout << "Epochs, bestAnswer, Size" << std::endl;
+    for(int i = 0; i < parcialResults.size(); i++){
+        std::cout << (i * 100) << ", " << parcialResults[i] << ", " << graphSize << std::endl;
+    }
     return 0;
 }
